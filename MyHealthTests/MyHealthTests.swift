a1bac990563fdc3b2b149a15609ddf260e84ba80
paulrobinson010 <41989376+paulrobinson010@users.noreply.github.com>
2026-08-24 19:48:13 +0100

    New test target

---
+++ MyHealthTests/MyHealthTests.swift
@@ -0,0 +1,18 @@
+//
+//  MyHealthTests.swift
+//  MyHealthTests
+//
+//  Created by Paul Robinson on 24/08/2026.
+//
+
+import Testing
+
+struct MyHealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
